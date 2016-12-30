687a09a94fde914d2a8603e8e88f1e5aca1c5a84
zhouguojing02 <zhouguojing02@meituan.com>
2016-12-30 20:05:16 +0800

    相应结果使用jq解析

--- mos-manage/mos_manage.sh
+++ mos-manage/mos_manage.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # 管理美团云主机的脚本
 
 
 . $(cd `dirname $0`; pwd)/basefunction.sh
 
-#$1 Action
+#$1 Action: request type name
 #$2 access key
 #$3 access secret
-response=`doPost $1 $2 $3`
-echo $response
+#response=`doPost $1 $2 $3 | jq .`
+doPost $1 $2 $3 | jq .
